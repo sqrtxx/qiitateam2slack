7e6c340b4880e0f2836ac4ca9e3390cf5e41f0eb
sqrtxx <sqrtxx@gmail.com>
2014-03-26 19:43:35 +0900

    Add flowdock.rb

---
+++ qiitateam2flowdock.rb
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+require 'qiita'
+require 'yaml'
+require 'net/http'
+require 'uri'
+require 'json'
+require 'time'
+require 'flowdock'
+
+SETTINGS = YAML.load_file("./settings.yml")
+qiita = Qiita.new token: SETTINGS['qiita_token_key']
+
+def post(text, subject)
+  flow = Flowdock::Flow.new(:api_token => SETTINGS['flowdock_api_token'], :source => "Qiita:Team", :from => {:name => "Qiita:Team", :address => SETTINGS['flowdock_address']})
+  flow.push_to_team_inbox(:content => text, :subject => subject, :source => "Qiita:Team")
+end
+
+SETTINGS['qiita_user_list'].each do |user|
+  items = qiita.user_items user, team_url_name: SETTINGS['qiita_team_name']
+  items.select {|item| Time.parse(item.created_at) > (Time.now - SETTINGS['cron_interval'] * 60)}.each do |item|
+    text = <<-EOF
+#{item.user.url_name} wrote #{item.title} #{item.url}
+at #{item.created_at_in_words}
+EOF
+    puts "try post"
+    puts text, item.title
+    post(text, item.title)
+  end
+end
+
+
